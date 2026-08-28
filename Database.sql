562a67a7b362511e42ab28a902a4adb9a4e0520f
Barthelemy Nsiala Musongo <barthelemynsiala23@gmail.com>
2026-08-28 14:04:44 +0100

    Table utilisateur's creation

--- Database.sql
+++ Database.sql
@@ -1,1 +1,15 @@
 CREATE DATABASE kadea_chat_database;
+
+CREATE TABLE utilisateur (
+    id_utilisateur SERIAL PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    mot_de_passe VARCHAR(255) NOT NULL,
+    telephone VARCHAR(100),
+    langue VARCHAR(100),
+    avatar VARCHAR(255),
+    bio VARCHAR(255),
+    date_creation TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    date_modification TIMESTAMP
+);
